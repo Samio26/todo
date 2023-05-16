4ce9c8173b4c461d1a8372a24719b62dbf038aeb
issasahbi <sahbi-issa@hotmail.com>
2023-05-16 17:35:13 +0100

    add unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,72 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:todo/di/injection.dart';
+import 'package:todo/feature_tasks/domain/model/task.dart';
+import 'package:todo/feature_tasks/domain/repository/home_repository.dart';
 
 import 'package:todo/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+ TestWidgetsFlutterBinding.ensureInitialized();
+    final HomeRepository _repo=getIt<HomeRepository>();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  group('Create Task', () {
+    test('Task should be created in Firestore', () async {
+      // Initialize Firestore
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      // Set up the data for the task
+      
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      await _repo.createTask(Taskk(
+        categ: "Work",
+        title: "title",
+        date: "06/03/23",
+        desc: "fjdjfdjfdjfjdfd"
+      ));
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      QuerySnapshot snapshot = await firestore.collection('Task').get();
+
+      expect(snapshot.docs.length, 1);
+
+      DocumentSnapshot doc = snapshot.docs[0];
+      expect(doc.data(), 'Test Task');
+    });
+  });
+
+    group('Get All Tasks', () {
+    test('Should retrieve all tasks from Firestore', () async {
+      // Initialize Firestore
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+      // Add dummy tasks to Firestore for testing
+      await firestore.collection('tasks').add({'title': 'Task 1'});
+      await firestore.collection('tasks').add({'title': 'Task 2'});
+      await firestore.collection('tasks').add({'title': 'Task 3'});
+
+      // Retrieve all tasks
+      List<Taskk> tasks = (await _repo.getAllTasks()) as List<Taskk>;
+
+      // Check the number of tasks retrieved
+      expect(tasks.length, 3);
+
+      // Verify the task titles
+      expect(tasks[0], 'Task 1');
+      expect(tasks[1], 'Task 2');
+      expect(tasks[2], 'Task 3');
+    });
   });
 }
+
+
+
+
+
+
+
+
+
+
